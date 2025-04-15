536ad9099b74bf2b8d5f98b29827014f4af0181d
jie <804957280@qq.com>
2025-04-15 20:04:14 +0800

    双人，移动

---
+++ Assets/Scripts/PlayerControllerForPlayer2.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerForPlayer2 : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
+    private Vector2 InputDirection;
+    private PlayerStatus status;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+        rb = GetComponent<Rigidbody2D>();
+        status = GetComponent<PlayerStatus>();
+    }
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDirection = inputControl.GamePlay2.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMove();
+    }
+
+    public void HandleMove()//双人：使用inputSystem
+    {
+        rb.velocity = new Vector2(InputDirection.x * status.speed * Time.deltaTime, InputDirection.y * status.speed * Time.deltaTime);
+
+        //翻转
+        int faceDir = (int)transform.localScale.x;
+        if (InputDirection.x > 0) faceDir = 1;
+        if (InputDirection.x < 0) faceDir = -1;
+
+        gameObject.transform.localScale = new Vector3(faceDir, 1, 1);
+    }
+}
---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int health;
+    public int attackForce;
+    public int defenceForce;
+    public float speed;
+    public float attackSpeed;
+    private float attackRate;
+}
