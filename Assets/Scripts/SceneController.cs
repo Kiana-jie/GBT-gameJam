5b7c76ca7fdb703d6d34337688f8a88d4c7f4f7c
JustinWCola <867581856@qq.com>
2025-04-19 18:51:10 +0800

    fix:解决两场景直接切换时会穿帮

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,28 +9,29 @@ public class SceneController : MonoBehaviour
     public GameObject wall;
     public GameObject player1Mask;
     public GameObject player2Mask;
-    public Vector3 resetPosition;
+    public Vector3 centrePosition;
     public Vector3 world1Position;
     public Vector3 world2Position;
+    private int worldNow = 0;
     // Start is called before the first frame update
     void Start()
     {
-        player1Mask.SetActive(false);
-        player2Mask.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 测试场景切换，用123左右切
         if (Input.GetKeyDown(KeyCode.Alpha1))
-            ShiftToWorld1();
+            StartCoroutine(ShiftToWorld1());
         if (Input.GetKeyDown(KeyCode.Alpha2))
-            ShiftToCentre();
+            StartCoroutine(ShiftToCentre());
         if (Input.GetKeyDown(KeyCode.Alpha3))
-            ShiftToWorld2();
-
+            StartCoroutine(ShiftToWorld2());
     }
 
+    // 协程，将折叠线移动到任意位置
     private IEnumerator MoveWall(Vector3 targetPosition, float duration)
     {
         Vector3 startPosition = wall.transform.position;
@@ -45,28 +46,9 @@ public class SceneController : MonoBehaviour
         }
 
         wall.transform.position = targetPosition;
-        wall.GetComponent<BoxCollider2D>().enabled = true;
     }
 
-    private IEnumerator ResetWall(float duration)
-    {
-        Vector3 startPosition = wall.transform.position;
-        float time = 0.0f;
-
-        while (time < duration)
-        {
-            float t = time / duration;
-            wall.transform.position = Vector3.Lerp(startPosition, resetPosition, t);
-            time += Time.deltaTime;
-            yield return null;
-        }
-
-        wall.transform.position = resetPosition;
-        wall.GetComponent<BoxCollider2D>().enabled = true;
-        StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
-        StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
-    }
-
+    // 协程，将折叠线移动到任意位置，并旋转到任意角度，虽然暂时不用但是以后可以拿来玩
     public IEnumerator MoveWall(Vector3 targetPosition, Quaternion targetRotation, float duration)
     {
         Vector3 startPosition = wall.transform.position;
@@ -84,32 +66,64 @@ public class SceneController : MonoBehaviour
 
         wall.transform.position = targetPosition;
         wall.transform.rotation = targetRotation;
+    }
+
+    // 没救了，世界1和世界2之间不能直接切，会穿帮
+    // 我直接暴力解决
+
+    // 协程队列，切换到世界1
+    public IEnumerator ShiftToWorld1()
+    {
+        wall.GetComponent<BoxCollider2D>().enabled = false;
+        if (worldNow == 0)
+        {
+            StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().EnableMask(0.1f));
+            yield return StartCoroutine(MoveWall(world1Position, 0.5f));
+            yield return StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
+        }
+        else if (worldNow == 2)
+        {
+            yield return StartCoroutine(MoveWall(centrePosition, 0.25f));
+            StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().DisableMask(0.05f));
+            StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().EnableMask(0.05f));
+            yield return StartCoroutine(MoveWall(world1Position, 0.25f));
+            yield return StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
+        }
         wall.GetComponent<BoxCollider2D>().enabled = true;
+        worldNow = 1;
     }
 
-
-    public void ShiftToWorld1()
+    // 协程队列，切换到世界2
+    public IEnumerator ShiftToWorld2()
     {
         wall.GetComponent<BoxCollider2D>().enabled = false;
-        player1Mask.SetActive(true);
-        player2Mask.SetActive(false);
-        StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().EnableMask(0.1f));
-        StartCoroutine(MoveWall(world1Position, 0.5f));
+        if (worldNow == 0)
+        {
+            StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().EnableMask(0.1f));
+            yield return StartCoroutine(MoveWall(world2Position, 0.5f));
+            yield return StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
+        }
+        else if (worldNow == 1)
+        {
+            yield return StartCoroutine(MoveWall(centrePosition, 0.25f));
+            StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().DisableMask(0.05f));
+            StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().EnableMask(0.05f));
+            yield return StartCoroutine(MoveWall(world2Position, 0.25f));
+            yield return StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
+        }
+        wall.GetComponent<BoxCollider2D>().enabled = true;
+        worldNow = 2;
     }
 
-    public void ShiftToWorld2()
+    // 协程队列，切换到中间
+    public IEnumerator ShiftToCentre()
     {
         wall.GetComponent<BoxCollider2D>().enabled = false;
-        player1Mask.SetActive(false);
-        player2Mask.SetActive(true);
-        StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().EnableMask(0.1f));
-        StartCoroutine(MoveWall(world2Position, 0.5f));
-    }
-
-    public void ShiftToCentre()
-    {
-        StartCoroutine(ResetWall(0.5f));
-
+        yield return StartCoroutine(MoveWall(centrePosition, 0.5f));
+        StartCoroutine(player1Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
+        StartCoroutine(player2Mask.GetComponent<PlayerMaskController>().DisableMask(0.1f));
+        wall.GetComponent<BoxCollider2D>().enabled = true;
+        worldNow = 0;
     }
 
 }
